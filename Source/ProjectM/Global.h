57f025a662900eebb2d8b41ca55a6794150c19cb
PHSNB\gws10 <gws1017@naver.com>
2023-03-11 15:58:22 +0900

    개발시 유용하거나 편의를 위한 class 및 코드 추가
    - visual studio intergration tool 플러그인 추가
    - include시 상대경로를 쓰기위해 Build.cs에 코드 추가함
    - 자주 사용되는 코드 static함수로 UHelepers에 등록
    - Global 헤더 추가

---
+++ Source/ProjectM/Global.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "KismetAnimationLibrary.h"
+
+#include "Utilities/UHelpers.h"
+
+
---
+++ Source/ProjectM/Utilities/UHelpers.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "UObject/ConstructorHelpers.h"
+
+
+class ProjectM_API UHelpers
+{
+public:
+	template<typename T>
+	static void GetAsset(T** OutObject, FString InPath)
+	{
+		ConstructorHelpers::FObjectFinder<T> asset(*InPath);
+		verifyf(asset.Succeeded(), L"asset.Succeeded()");
+		*OutObject = asset.Object;
+	}
+
+	template<typename T>
+	static void CreateComponent(AActor* InActor, T** InComponent,
+		FName InName, USceneComponent* InParent = NULL)
+	{
+		*InComponent = InActor->CreateDefaultSubobject<T>(InName);
+
+		if (!!InParent)
+		{
+			(*InComponent)->SetupAttachment(InParent);
+			return;
+		}
+
+		InActor->SetRootComponent((*InComponent));
+	}
+
+	template<typename T>
+	static void GetClass(TSubclassOf<T>* OutClass, FString InPath)
+	{
+		ConstructorHelpers::FClassFinder<T> asset(*InPath);
+
+		verifyf(asset.Succeeded(), L"asset.Succeeded()");
+
+		*OutClass = asset.Class;
+	}
+
+
+};
